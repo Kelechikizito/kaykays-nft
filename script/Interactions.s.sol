328864a8316e5e3b88dc12cc2ec7c73dcdba5833
kelechikizito <kaylaychi77@gmail.com>
2025-05-18 14:57:03 +0100

    update: interactions and tokenURI

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+// https://ipfs.io/
+
 import {Script} from "forge-std/Script.sol";
 import {KayKayNFT} from "src/KayKayNFT.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 contract MintKayKayNFT is Script {
     string public constant KAYKAY_URI =
-        "https://ipfs.io/ipfs/QmUJ2qNuXbsVoVmi9jEiMv4sQR4jkMEU3QdtFxnncP7aWC/";
+        "ipfs://QmdeL6H4DWkk6cB9piDD73tirropWnJEX2bkCiEvQ33asj";
 
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
